41e8cf03da3da97c084df843f08e3660bf666912
Bilal Shuja <bilalshuja77@gmail.com>
2022-09-10 17:53:09 +0500

    git-testing

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <form action="">
+            <label htmlFor="">
+                Name:
+            </label>
+            <input type="text" />
+            <label htmlFor="">
+                Password:
+            </label>
+            <input type="text" />
+        </form>
+    </div>
+  )
+}
+
+export default Header
